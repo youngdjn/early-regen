74699240fc23a86bc6e8f28c14ab86bc07d43fa7
Derek Young <derek.j.n.young@gmail.com>
2022-04-28 19:42:41 -0700

    Save point files for diff species, plot colors

--- scripts/data-carpentry/prep-geospatial.R
+++ scripts/data-carpentry/prep-geospatial.R
@@ -18,15 +18,58 @@ d = st_as_sf(d,coords=c("lon","lat"), crs=4326)
 
 ## write all plots
 
-st_write(d, datadir("field-data/processed/spatial/all_plots.gpkg"))
+st_write(d, datadir("field-data/processed/spatial/all_plots.gpkg"), delete_dsn = TRUE)
 
-### core pine plots
+### core nogrn pine plots
 
 core_nogrn_pine = d %>%
   filter(toupper(plot_type) == "CORE") %>%
   filter(dist_grn_PINES > 100 | is.na(dist_grn_PINES))
 
-st_write(core_nogrn_pine, datadir("field-data/processed/spatial/core_nogrn_pine.gpkg"))
+st_write(core_nogrn_pine, datadir("field-data/processed/spatial/core_nogrn_pine.gpkg"), delete_dsn = TRUE)
+
+## core grn pine plots 
+
+core_nogrn_pine = d %>%
+  filter(toupper(plot_type) == "CORE") %>%
+  filter(dist_grn_PINES < 50)
+
+st_write(core_nogrn_pine, datadir("field-data/processed/spatial/core_grn_pine.gpkg"), delete_dsn = TRUE)
+
+## seedwall pine plots
+
+core_nogrn_pine = d %>%
+  filter(toupper(plot_type) == "SEEDWALL")
+
+st_write(core_nogrn_pine, datadir("field-data/processed/spatial/seedwall_pine.gpkg"), delete_dsn = TRUE)
+
+
+
+#### ALL SPP
+
+### core nogrn all plots
+
+core_nogrn_pine = d %>%
+  filter(toupper(plot_type) == "CORE") %>%
+  filter(dist_grn_ALL > 100 | is.na(dist_grn_ALL))
+
+st_write(core_nogrn_pine, datadir("field-data/processed/spatial/core_nogrn_all.gpkg"), delete_dsn = TRUE)
+
+## core grn all plots 
+
+core_nogrn_pine = d %>%
+  filter(toupper(plot_type) == "CORE") %>%
+  filter(dist_grn_ALL < 50)
+
+st_write(core_nogrn_pine, datadir("field-data/processed/spatial/core_grn_all.gpkg"), delete_dsn = TRUE)
+
+## seedwall all plots
+
+core_nogrn_pine = d %>%
+  filter(toupper(plot_type) == "SEEDWALL")
+
+st_write(core_nogrn_pine, datadir("field-data/processed/spatial/seedwall.gpkg"), delete_dsn = TRUE)
+
 
 
 
