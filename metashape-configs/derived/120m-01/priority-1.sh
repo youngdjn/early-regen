5ae204c61e17b83847a91a088baa41fa8d930df6
Derek Young <djyoung@ucdavis.edu>
2023-03-05 00:03:27 +0000

    Add derived configs

---
+++ metashape-configs/derived/120m-01/priority-1.sh
@@ -0,0 +1,3 @@
+python /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/early-regen/metashape-configs/derived/120m-01/Caldor-C013-S012.yml
+python /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/early-regen/metashape-configs/derived/120m-01/Dixie-C03S04.yml
+python /ofo-share/utils/automate-metashape/python/metashape_workflow.py /ofo-share/repos-derek/early-regen/metashape-configs/derived/120m-01/Dixie-C13S14.yml
