a3a4463c895b7eeea4825b9d12005a4b54fdec4e
Derek Young <derek.j.n.young@gmail.com>
2023-06-03 22:56:50 -0700

    Save plot attributes as yaml text to kml description field

--- scripts/data-carpentry/prep-previous-data-for-revisit-2023.R
+++ scripts/data-carpentry/prep-previous-data-for-revisit-2023.R
@@ -14,13 +14,17 @@ source(here("scripts/convenience_functions.R"))
 d = read_csv(datadir("field-data/processed/plot-data-prepped.csv")) |>
   mutate(across(contains("_dens_"), ~round(., digits = 2)))
 
+# turn all column names and values into a paragraph description
+for(i in 1:length(d)) {
+  d[i, "Description"] =  yaml:: as.yaml(d[i,])
+}
 
 d_verbose = d |>
-  select(fire, plot_id, plot_type, dist_grn_ALL, starts_with("radius_"), starts_with("vol_"), starts_with("seedl_dens_"), starts_with("dist_grn_"), fire_intens, day_of_burning, shrub_cover, sight_line, lat, lon) |>
+  select(fire, plot_id, plot_type, dist_grn_ALL, starts_with("radius_"), starts_with("vol_"), starts_with("seedl_dens_"), starts_with("dist_grn_"), fire_intens, day_of_burning, shrub_cover, sight_line, Description, lat, lon) |>
   arrange(fire, plot_id)
 
 # Write this for Derek reference in field
-write_csv(d_verbose, file.path(data_dir, "resurvey-prep/previous-plot-summary_verbose.csv"))
+write_csv(d_verbose |> select(-Description), file.path(data_dir, "resurvey-prep/previous-plot-summary_verbose.csv"))
 
 d_minimal = d |>
   select(fire, plot_id, plot_type, starts_with("radius_")) |>
@@ -29,17 +33,20 @@ d_minimal = d |>
 # Write this into a table to print for crew
 write_csv(d_minimal, file.path(data_dir, "resurvey-prep/previous-plot-summary_for-crew.csv"))
 
+
+
 # Also make it a kml for Avenza: all and separate for seed wall, core, delayed
 d_geo = d |>
-  select(plot_id, plot_type, starts_with("radius_"), lat, lon) |>
   mutate(Name = plot_id) |>
-  select(Name, everything()) |>
-  arrange(plot_id)
+  select(Name, Description, lat, lon, plot_type) |>
+  arrange(Name)
+
+
 d_geo = st_as_sf(d_geo, coords = c("lon", "lat"), crs = 4326)
 
-d_geo_sw = d_geo |> filter(plot_type == "seedwall")
-d_geo_core = d_geo |> filter(plot_type == "core")
-d_geo_delayed = d_geo |> filter(plot_type == "delayed")
+d_geo_sw = d_geo |> filter(plot_type == "seedwall") |> select(-plot_type)
+d_geo_core = d_geo |> filter(plot_type == "core") |> select(-plot_type)
+d_geo_delayed = d_geo |> filter(plot_type == "delayed") |> select(-plot_type)
 
 st_write(d_geo, file.path(data_dir, "resurvey-prep/previous-plots_all.kml"), delete_dsn = TRUE)
 st_write(d_geo_sw, file.path(data_dir, "resurvey-prep/previous-plots_seedwall.kml"), delete_dsn = TRUE)
@@ -53,7 +60,7 @@ d_exemp = d_verbose |>
          dist_grn_ALL > 100 | is.na(dist_grn_ALL),
          seedl_dens_ALL > 0.1) |>
   mutate(Name = plot_id) |>
-  select(Name, everything())
+  select(Name, Description, lat, lon)
 
 # Write to KML for Avenza in the field
 d_geo = st_as_sf(d_exemp, coords = c("lon", "lat"), crs = 4326)
