7b557a545f7037b5c1808c35504dea5e05010a03
Derek Young <22568494+youngdjn@users.noreply.github.com>
2024-07-28 16:58:16 -0700

    Shrub clearing plot selection

--- scripts/analysis/look_for_2nd_yr_recruits_in_core.R
+++ scripts/analysis/look_for_2nd_yr_recruits_in_core.R
@@ -2,6 +2,7 @@
 
 
 library(tidyverse)
+library(sf)
 
 d = read_csv("~/Downloads/plot_data_2023.csv")
 
@@ -29,10 +30,72 @@ d2 = d |>
 
 d3 = d2 |>
     select(Fire, plot_id, seedl_dens_ALL, dist_grn_ALL, sight_line, lat, lon) |>
-    arrange(-seedl_dens_ALL)
+    arrange(-seedl_dens_ALL) |>
+    mutate(seedl_dens_ALL = ifelse(is.na(seedl_dens_ALL), 0, seedl_dens_ALL)) |>
+    filter(!is.na(lat))
 
 nonzero_seedl = d3$seedl_dens_ALL[d3$seedl_dens_ALL > 0]
 median(nonzero_seedl, na.rm = TRUE)
+
+dsp = st_as_sf(d3, coords = c("lon", "lat"), crs = 4326)
+
+st_write(dsp, "~/Downloads/seedl_dens_2023_formapping.gpkg")
+
+### Save just the selected core plots, with their new IDs
+
+core_selected = d3 |>
+    mutate(plot_prefix = recode(plot_id,
+                            "C041-160" = "T1",
+                            "C041-163" = "T1",
+                            "C041-167" = "T1",
+                            "C041-159" = "T1",
+                            "D042-505" = "T2",
+                            "C010-503" = "T2",
+                            "C006-044" = "T1",
+                            "C006-046" = "T2",
+                            "C003-015" = "T1",
+                            "C003-014" = "T1",
+                            "C003-013" = "T2",
+                            "C003-017" = "T2",
+                            .default = "")) |>
+    filter(plot_prefix != "") |>
+    mutate(new_plot_id = paste0(plot_prefix, "_", plot_id)) |>
+    select(Name = new_plot_id, everything()) |>
+    st_as_sf(coords = c("lon", "lat"), crs = 4326)
+
+st_write(core_selected, "~/Downloads/shrubclearing_core_selected.kml", delete_dsn = TRUE)
+
+### Save all core plots, minus these
+
+core_unselected = d3 |>
+    filter(!(plot_id %in% core_selected$plot_id)) |>
+    select(Name = plot_id, everything()) |>
+    st_as_sf(coords = c("lon", "lat"), crs = 4326)
+
+st_write(core_unselected, "~/Downloads/shrubclearing_core_unselected.kml", delete_dsn = TRUE)
+
+### Save all the seed wall plots
+
+seedwall_selected = d |>
+    filter(str_sub(plot_id, 1, 1) == "S",
+           dist_grn_ALL < 60,
+           Fire == "Caldor",
+           seedl_dens_ALL > 0.003) |>
+    select(Name = plot_id, everything()) |>
+    st_as_sf(coords = c("lon", "lat"), crs = 4326)
+    
+st_write(seedwall_selected, "~/Downloads/shrubclearing_seedwall_selected.kml", delete_dsn = TRUE)
+
+seedwall_unselected = d |>
+    filter(str_sub(plot_id, 1, 1) == "S",
+           dist_grn_ALL < 60,
+           Fire == "Caldor") |>
+    filter(seedl_dens_ALL <= 0.003 | is.na(seedl_dens_ALL)) |>
+    select(Name = plot_id, everything()) |>
+    st_as_sf(coords = c("lon", "lat"), crs = 4326)
+
+st_write(seedwall_unselected, "~/Downloads/shrubclearing_seedwall_unselected.kml", delete_dsn = TRUE)
+
 
 ## What is seed wall seedling density?
 d4 = d |>
